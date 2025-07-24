0938116538f7e665c4d309116ada601e59a3724a
Betojr04 <betomasia12@gmail.com>
2025-07-24 16:20:05 -0700

    finished first version of the landing page need to finalize style but will be uploading the website to render already

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-from app import db
-from flask_migrate import Migrate
+from app import create_app, db
 
-migrate = Migrate(app, db)
+app = create_app()
 
 
+# Optional: Flask CLI context for shell
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
